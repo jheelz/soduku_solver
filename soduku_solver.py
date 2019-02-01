1b115868fd1a027c6e2129c841b0ff7d70f882dd
James Healy <james_healy@Jamess-MacBook-Air.local>
2019-01-31 18:38:21 -0800

    almost finished rough draft of soduku solver

--- soduku_solver.py
+++ soduku_solver.py
@@ -9,24 +9,85 @@ class entry(row, col):
     self.legal_moves = soduku.
 '''
 #inspired by depth search algorithm
-class soduku:
 
-    #need to get available moves that satisfy rules; each number can be used 9 times
-    #rules: number can 1) only appear once across 1x9 row, 2) once across 9x1 column, and 3) once in 3x3 section
-    def __init__(self):
+def soduku_board(filename):
 
-        #initialize matrix of zeroes and a matrix containing fixed values
-        self.variable_map = np.zeros((9,9))
-        self.fixed_map = np.array(file)
+    fixed_map = np.read(filename)
+    variable_map = np.zeros((9,9)) + fixed_map
 
-        #initialize dictionary containing how many times a soduku entry (1,2,...) is left
-        self.soduku_entrys_left = {0:9, 1:9, 2:9, 3:9, 4:9, 5:9, 6:9, 7:9, 8:9, 9:9}
+    soduku_entrys_left = {0:9, 1:9, 2:9, 3:9, 4:9, 5:9, 6:9, 7:9, 8:9, 9:9}
 
-        #initialize tree nodes
-        self.soduku_nodes = []
+    soduku_nodes = {}
+
+    #helper function to get matrix position of integer (0 -> [0,0], 9 -> [1,0])
+    def placement(x):
+        row = (x + 9)/9 - 1
+        col = x % 9
+        return [row,col]
+
+    def get_node_options(soduku_cells_filled):
+        pos = placement(soduku_cells_filled)
+
+        #return fixed numbers
+        if fixed_map[pos] <> 0:
+            return [fixed_map[pos]]
+
+        col = variable_map[:, pos[1]]
+
+        #get nums taken in row
+        row = variable_map[pos[0], :]
+
+        #get nums taken in block
+        block_row = pos[0] / 3
+        block_column = pos[1] / 3
+
+        block = variable_map[block_row * 2 : block_row * 2 + 2, block_column * 2 : block_column * 2 + 2]
+
+        soduku_nodes.append(list(set(list(np.unique(col)) + list(np.unique(row)) + list(np.unique(block)))))
+
+    def is_legal(node):
+        if (soduku_entry_left[node[-1]] - 1) >= 0:
+            return True
+        else:
+            return False
 
 
+    def solve(node):
+
+        if is_legal(soduku_nodes[soduku_cells_filled]):
+            variable_map[node cell] = node[-1]
+            soduku_entry_left[soduku_nodes[soduku_cells_filled][-1]] += -1
+            soduku_cells_filled += 1
+
+            #solved!
+            if soduku_puzzle_size == 80:
+                return variable_map
+            #else keep going
+            else:
+                solve(get_node_options(soduku_nodes[soduku_cells_filled]))
+
+        #if entry in next node is illegal
+        if !is_legal(node):
+            #remove entry
+
+            node = node[:-1]
+            #if node is empty after removing entry
+            if len(node) == 0:
+                #return value of prior cell to dictionary count
+                soduku_entry_left[soduku_nodes[soduku_cells_filled - 1][-1]] += 1
+
+                #alter prior node and remove tail of list
+                soduku_nodes[soduku_cells_filled - 1] = soduku_nodes[soduku_cells_filled - 1][:-1]
+
+                #back track node
+                soduku_cells_filled += -1
+                solve(soduku_nodes[soduku_cells_filled - 1])
 
 
-    def legal_numbers(self, cell):
-        
+            else:
+                solve(soduku_nodes[soduku_cells_filled])
+
+    soduku_cells_filled = 0
+    get_node_options
+    #initialize first soduku_node
+    solve(first_node)
